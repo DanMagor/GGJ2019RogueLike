916e0f9bb8af370da5bd8e38d2c031405a7f61da
DanMagor <anybis-maat@mail.ru>
2019-01-26 00:42:22 +0300

    draft light on/of implemented

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+
+    public float speed = 1;
+
+    public PlayerController player;
+
+    public GameManager gameManager;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.LightIsOn)
+        {
+            transform.position += (player.transform.position - transform.position).normalized * speed * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 100;
+
+    public Image darkScreen;
+
+    [SerializeField]
+    private GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3(speed * Time.deltaTime,0,0) * Input.GetAxis("Horizontal") +
+                              new Vector3(0, speed * Time.deltaTime, 0) * Input.GetAxis("Vertical"); 
+        if (Input.GetButtonDown("Jump"))
+        {
+            darkScreen.enabled = !darkScreen.enabled;
+            gameManager.LightIsOn = !darkScreen.enabled;
+        }
+	}
+}
