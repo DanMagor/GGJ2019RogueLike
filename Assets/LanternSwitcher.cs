882cffb66a92480f80825172b54222f3633ce416
DanMagor <anybis-maat@mail.ru>
2019-01-26 22:55:16 +0300

    tutorials room2

--- Assets/LanternSwitcher.cs
+++ Assets/LanternSwitcher.cs
@@ -2,49 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LanternSwitcher: MonoBehaviour {
+public class LanternSwitcher : MonoBehaviour
+{
 
-    public bool LightIsOn() {
+    public bool LightIsOn()
+    {
         return flashlight.activeSelf;
     }
 
-	void Start () {
-		flashlight = transform.Find("Flashlight").gameObject;
-		lightnessHighlight = transform.Find("LightnessHighlight").gameObject;
-		darknessHighlight = transform.Find("DarknessHighlight").gameObject;
-	}
-	
-	void Update () {
-		var angle = getRotationAngle();
-		const float RotationSpeed = 90.0f;
-		
-		if (angle > 5) 
-			transform.Rotate(RotationSpeed * Vector3.forward * Time.deltaTime);
-		else if (angle < -5) {
-			transform.Rotate(-RotationSpeed * Vector3.forward * Time.deltaTime);
-		}
+    void Start()
+    {
+        flashlight = transform.Find("Flashlight").gameObject;
+        lightnessHighlight = transform.Find("LightnessHighlight").gameObject;
+        darknessHighlight = transform.Find("DarknessHighlight").gameObject;
+    }
 
-		if (Input.GetKeyDown("space")) {
-			if(flashlight.activeSelf) {
-				flashlight.SetActive(false);
-				lightnessHighlight.SetActive(false);
-				darknessHighlight.SetActive(true);
-			} else if(darknessHighlight.activeSelf) {
-				flashlight.SetActive(true);
-				lightnessHighlight.SetActive(true);
-				darknessHighlight.SetActive(false);
-			}
-		}
-	}
-
-	private float getRotationAngle() {
+    void Update()
+    {
         var mousePos = Input.mousePosition;
-		var point = Camera.main.ScreenToWorldPoint(
-			new Vector3(mousePos.x, mousePos.y, 10));
-		return Vector3.SignedAngle(transform.right, point - transform.position, Vector3.forward);
-	}
+        var point = Camera.main.ScreenToWorldPoint(
+          new Vector3(mousePos.x, mousePos.y, 10));
+        flashlight.transform.LookAt(point);
 
-	private GameObject flashlight;
-	private GameObject lightnessHighlight;
-	private GameObject darknessHighlight;
+        if (Input.GetKeyDown("space"))
+        {
+            if (flashlight.activeSelf)
+            {
+                flashlight.SetActive(false);
+                lightnessHighlight.SetActive(false);
+                darknessHighlight.SetActive(true);
+            }
+            else if (darknessHighlight.activeSelf)
+            {
+                flashlight.SetActive(true);
+                lightnessHighlight.SetActive(true);
+                darknessHighlight.SetActive(false);
+            }
+        }
+    }
+
+    private GameObject flashlight;
+    private GameObject lightnessHighlight;
+    private GameObject darknessHighlight;
 }
