314042cfe2fcc6c582b6a1a32e7055471e9826e5
DanMagor <anybis-maat@mail.ru>
2019-01-27 10:36:01 +0300

    Added UpRoom

--- Assets/DireCorpseController.cs
+++ Assets/DireCorpseController.cs
@@ -2,38 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DireCorpseController: MonoBehaviour {
+public class DireCorpseController : MonoBehaviour
+{
     public float Speed = 15.0f;
 
+
+    private Animator animatorController;
     GameObject player;
 
-    void Start() {
+    void Start()
+    {
         player = GameObject.FindWithTag("Player");
         lastPlayerPosition = transform.position;
+        animatorController = gameObject.GetComponent<Animator>();
     }
 
-    void Update() {
+    void Update()
+    {
         bool huntIsOn = player.GetComponent<LanternSwitcher>().LightIsOn();
-        
-        if(huntIsOn) {
+
+        if (huntIsOn)
+        {
             approachPlayer();
         }
     }
 
-    private void approachPlayer() {
-        if (isPlayerVisible()) {
+    private void approachPlayer()
+    {
+        if (isPlayerVisible())
+        {
             lastPlayerPosition = player.transform.position;
         }
-        
+
+        if (lastPlayerPosition.x > transform.position.x)
+        {
+            animatorController.SetBool("MoveRight", true);
+        }
+        else
+        {
+            animatorController.SetBool("MoveRight", false);
+        }
         transform.position = Vector3.MoveTowards(transform.position, lastPlayerPosition, Speed * Time.deltaTime);
     }
 
-    private bool isPlayerVisible() {        
+    private bool isPlayerVisible()
+    {
         var dir = player.transform.position - transform.position;
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100.0f, LayerMask.GetMask("Wall"));
         return !hit;
-        
+
     }
 
     private Vector3 lastPlayerPosition;
