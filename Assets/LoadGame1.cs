c9183a9f2b46f698bc96b89848fc3dfca884c227
Harrm <harrm@mail.ru>
2019-01-27 12:40:35 +0300

    Intro

---
+++ Assets/LoadGame1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadGame1: MonoBehaviour {
+    public GameObject TransitionPanel;
+
+    public void load() {
+        StartCoroutine("FadeAndLoad");
+    }
+
+    public IEnumerator FadeAndLoad() {
+        for (float f = 1f; f >= 0; f -= 0.1f) {
+            Color c = TransitionPanel.GetComponent<Image>().color;
+            c.r = f;
+            c.b = f;
+            c.g = f;
+            c.a = 1 - f;
+            TransitionPanel.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        SceneManager.LoadScene("Game1");
+    }
+
+}
---
+++ Assets/Transition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Transition : MonoBehaviour {
+    
+    public GameObject TransitionPanel;
+    public GameObject NextPanel;
+
+    public void BeginTransition() {
+        StartCoroutine("StartTransition");
+    }
+
+
+
+    IEnumerator StartTransition() {
+        for (float f = 0f; f <= 2; f += 0.1f) {
+            Color c = TransitionPanel.GetComponent<Image>().color;
+            c.a = f;
+            TransitionPanel.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        NextPanel.SetActive(true);
+        for (float f = 1f; f >= 0; f -= 0.1f) {
+            Color c = TransitionPanel.GetComponent<Image>().color;
+            c.a = f;
+            TransitionPanel.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        TransitionPanel.SetActive(false);
+
+    }
+}
